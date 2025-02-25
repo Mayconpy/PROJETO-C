a241e5a62408cfa9c1fd7ccb98e73b278b1e5a42
mayconK <kmaycon924@gmail.com>
2025-02-25 15:25:40 -0300

    organizing project

--- Main.c
+++ Main.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
-void a();
+
+void Basic_optimization(){}
+
+void optization_intermediary(){}
+
+void advanced_optimization(){}
+
+void colortext(int color){SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),color);}
+
 int main(){
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-
-    int choice;
-    printf("Escolha qual meio de otimizacao voce quer\n1-Otimizacao fraca\n2-otimizacao media\n3-otimizacao forte: ");
-
-    
-    SetConsoleTextAttribute(hConsole,FOREGROUND_BLUE);
+    colortext(1);
     printf("\notimizacao fraca ira desabilitar  aplicativos em segundo plano, ativar modo jogo\n");
-    
-    SetConsoleTextAttribute(hConsole,FOREGROUND_GREEN);
+
+    colortext(2);
     printf("\nOtimizacao media ira fazer tudo oque a otimizacao fraca faz porem ira tambem mudar seu plano de energia do pc ou nootbook\n");
-    
-    SetConsoleTextAttribute(hConsole,FOREGROUND_RED);
-    printf("\notimizacao forte ira fazer tudo isso e tambem desabilitar servicos inuteis e alguns deixar manual irei citar alguns logo a baixo");
+
+    colortext(4);
+    printf("\notimizacao forte ira fazer tudo isso e tambem desabilitar servicos inuteis\ne alguns deixar manual irei citar alguns logo a baixo");
+
+
+
+
+    colortext(7);
+    int choice;
+    printf("\nEscolha qual meio de otimizacao voce quer\n1-Otimizacao fraca\n2-otimizacao media\n3-otimizacao forte: ");
 
 
     scanf("%d", &choice);
     switch (choice){
     case 1:
-    printf("s");
+    Basic_optimization();
     break;
-    
-    case 2: printf("print");break;
+
+    case 2: 
+    optization_intermediary();
+    break;
     }
 }
+
---
+++ advanced_optmization.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+
+void disable_Services();
+
+
+void activate_game_mode();
+
+void disable_background_apps();
+
+void change_power_plan();
+
+
+void disable_windows_defender();
+
+
+void delete_temp_paste();
+
+
