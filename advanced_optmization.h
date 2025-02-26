9176991e39aede486cc9d22f823404fb71860e1a
mayconK <kmaycon924@gmail.com>
2025-02-26 14:28:00 -0300

    Atualizaçao 26/02/25

--- advanced_optmization.h
+++ advanced_optmization.h
@@ -3,7 +3,26 @@
 #include <windows.h>
 
 
-void disable_Services();
+void chartowchar(const char *srcm, wchar_t *dest, int destsize){
+    MultiByteToWideChar(CP_ACP,0,srcm,-1,dest,destsize);
+}
+
+void disable_Services(const char *serviceName){
+    wchar_t servicenamew[256];
+    chartowchar(serviceName,servicenamew,256);
+
+    //abre o gerenciador de controle de serviços com acesso total
+    SC_HANDLE hSCMANAGEr = OpenSCManager(NULL,NULL,SC_MANAGER_ALL_ACCESS);
+    if(!hSCMANAGEr){
+        printf("error ao abrir o arquivo %ld",GetLastError());
+        return;
+    }
+
+    SC_HANDLE Hservice  = OpenService(hSCMANAGEr,servicenamew,SERVICE_CHANGE_CONFIG| SERVICE_STOP);
+    
+
+
+}
 
 
 void activate_game_mode();
